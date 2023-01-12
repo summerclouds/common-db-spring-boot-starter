d8d4e47ed7c7cc820f9e22dbf232b31412bd1af1
Mike Hummel <mh@mhus.de>
2023-01-13 00:44:47 +0100

    0.0.3

--- src/main/java/org/summerclouds/common/db/xdb/XdbManager.java
+++ src/main/java/org/summerclouds/common/db/xdb/XdbManager.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 import org.summerclouds.common.core.activator.Activator;
 import org.summerclouds.common.core.cfg.BeanRefMap;
 import org.summerclouds.common.core.error.MException;
